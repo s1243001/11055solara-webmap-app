44ac570ae7f0e1bdfb491c7b57f5c4ecfee72b53
s1243001 <s1243001@gm.ncue.edu.tw>
2025-11-05 11:01:34 +0800

    Update 03_3D台北建築圖.py

--- pages/03_3D台北建築圖.py
+++ pages/03_3D台北建築圖.py
@@ -1,1 +1,23 @@
+import leafmap.maplibregl as leafmap
+import solara
 
+def create_map():
+
+    m = leafmap.Map(
+        center=[121.53545981563654, 25.0301736102832],
+        zoom=16,
+        pitch=60,
+        bearing=-17,
+        style="positron",
+        height="750px",
+        sidebar_visible=True,
+    )
+    m.add_basemap("Satellite", visible=False)
+    m.add_overture_3d_buildings(template="simple")
+    return m
+
+
+@solara.component
+def Page():
+    m = create_map()
+    return m.to_solara()
