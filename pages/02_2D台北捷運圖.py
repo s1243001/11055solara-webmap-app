a9c65f334c183818f89487f3f10363a9068e73b3
s1243001 <s1243001@gm.ncue.edu.tw>
2025-11-05 11:05:50 +0800

    Update 02_2D台北捷運圖.py

--- pages/02_2D台北捷運圖.py
+++ pages/02_2D台北捷運圖.py
@@ -5,12 +5,31 @@ import leafmap.maplibregl as leafmap
 def create_map():
 
     m = leafmap.Map(
-        style="liberty",
+        style="dark-matter",
         projection="globe",
         height="750px",
-        zoom=2.5,
+        center=[-100, 40],
+        zoom=4,
         sidebar_visible=True,
     )
+
+    MRT_geojson = ""
+    MRT_style = {
+        "layers": [
+            {
+                "id": "台北MRT",
+                "source": "transportation",
+                "source-layer": "segment",
+                "type": "line",
+                "paint": {
+                    "line-color": "#ffffff",
+                    "line-width": 2,
+                },
+            },
+        ]
+    }
+    
+    m.add_pmtiles(road_pmtiles, style=road_style, tooltip=True, fit_bounds=False)
     return m
 
 
